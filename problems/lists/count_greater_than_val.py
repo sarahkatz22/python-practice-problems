6ceffa1e3bf2a3ed29ce48a4b4d918cca8134548
sarahkatz22 <122055295+sarahkatz22@users.noreply.github.com>
2023-02-07 14:58:28 -0600

    Update count_greater_than_val.py

--- problems/lists/count_greater_than_val.py
+++ problems/lists/count_greater_than_val.py
@@ -8,6 +8,11 @@ def count_greater_than_val(lst, val):
     # After running your code, variable n should contain the value
     # we ask you to compute in this exercise.
     n = None
+    for num in lst: 
+        if num > val: 
+            n += 1
+        else: 
+            n += 0
 
     ### DO NOT MODIFY THE FOLLOWING LINE!
     return n
